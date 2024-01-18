f87bdfdba7e25d8b263a7a9fd7461639eb09b447
Slava <slavavetl@yahoo.com>
2024-01-18 16:36:27 +0200

    Send email

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
 st.header("Contact Me")
+
+with st.form(key="email_form"):
+    user_email = st.text_input("Your email address:")
+    message = st.text_area("Your message:")
+    button = st.form_submit_button("Send")
+
+    if button:
+        
---
+++ send_email.py
@@ -0,0 +1,23 @@
+import smtplib
+import ssl
+
+host = "smtp.gmail.com"
+port = 465
+
+
+username = 'slavavetl2@gmail.com'
+password = 'butltzczpnlniqnu'
+
+receiver = 'slavavetl2@gmail.com'
+context = ssl.create_default_context()
+
+message = """\
+Subject: Hi!
+How are you? 
+Bye!
+"""
+
+with smtplib.SMTP_SSL(host, port, context=context) as server:
+    server.login(username, password)
+    server.sendmail(username, receiver, message)
+
